259bf376fbbcc4f8ba2b4033afaccf9d0b807c22
Anandkr21 <anandkumargupta.akg15@gmail.com>
2023-09-14 00:38:54 +0530

    connected with mongoDB

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const { connection } = require('mongoose');
 const { app } = require('./app');
 const { allcourse } = require('./controller/courses');
 const { alluser } = require('./controller/user');
@@ -7,6 +8,13 @@ const Port = process.env.PORT || 8080;
 app.use('/user', alluser);
 app.use('/course', allcourse);
 
-app.listen(Port, () => {
+app.listen(Port, async () => {
+    try {
+        await connection
+        console.log("Connected with MongoDB");
+    } catch (error) {
+        console.log("Unable to connect with MongoDB");
+        console.error('Error:', error.message)
+    }
     console.log(`Server is running at http://localhost:${Port}`);
 });
